48948484d0f9c11684033ad17fa27908d8d3ab7b
Jeff Palm <jeff@jeffpalm.com>
2022-04-06 08:41:52 -0400

    update Wed Apr  6 08:41:52 EDT 2022

--- hackernewscolor/hackernewscolor.js
+++ hackernewscolor/hackernewscolor.js
@@ -1,29 +1,75 @@
-function hnMain() {
-  var divs = document.getElementsByTagName("SPAN");
+function colorScores() {
+  var spans = document.getElementsByTagName("SPAN");
   var scores = [];
   var min;
   var max;
-  for (var i=0; i<divs.length; i++) {
-    var d = divs[i];
+  for (var i = 0; i < spans.length; i++) {
+    var d = spans[i];
     if (d.id && d.id.match(/score_\d+/)) {
       var s = d.innerHTML;
       var res;
       if (res = s.match(/(\d+)/)) {
-	var n = res[0];
-	var score = parseInt(n);
-	scores.push(d);
-	if (!min || score<min) min = score;
-	if (!max || score>max) max = score;
+        var n = res[0];
+        var score = parseInt(n);
+        scores.push(d);
+        if (!min || score < min) min = score;
+        if (!max || score > max) max = score;
       }
     }
   }
-  for (var i=0; i<scores.length; i++) {
+  for (var i = 0; i < scores.length; i++) {
     var d = scores[i];
     var v = parseInt(d.innerHTML);
-    var cval = Math.floor(0xff - 0xee*(max-v)/(max-min));
+    var cval = Math.floor(0xff - 0xee * (max - v) / (max - min));
     var color = cval.toString(16) + "0000";
     d.style.color = "#" + color;
   }
+}
+
+function sortRows() {
+  let tab = document.querySelector('table[class="itemlist"]');
+  let tbody = tab.getElementsByTagName('tbody')[0];
+  let athings = Array.from(tbody.getElementsByClassName('athing'));
+  let findScore = tr => {
+    var spans = tr.getElementsByTagName("SPAN");
+    for (var i = 0; i < spans.length; i++) {
+      var d = spans[i];
+      if (d.id && d.id.match(/score_\d+/)) {
+        var s = d.innerHTML;
+        var res;
+        if (res = s.match(/(\d+)/)) {
+          var n = res[0];
+          var score = parseInt(n);
+          return score;
+        }
+      }
+    }
+  };
+  var athingsAndScores = [];
+  athings.forEach(tr => {
+    let score = findScore(tr.nextSibling);
+    console.log('score: ' + score);
+    athingsAndScores.push({
+      tr: tr,
+      next: tr.nextSibling,
+      score: score,
+    });
+  })
+  athingsAndScores.sort((a, b) => {
+    return b.score - a.score;
+  });
+  while (tbody.firstChild) {
+    tbody.removeChild(tbody.firstChild);
+  }
+  athingsAndScores.forEach(o => {
+    tbody.appendChild(o.tr);
+    tbody.appendChild(o.next);
+  });
+}
+
+function hnMain() {
+  colorScores();
+  sortRows();
 }
 
 hnMain();
